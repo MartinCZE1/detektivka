6c702d631d3e050bc8e9e8b18e7f0eb449204495
MartinCZE1 <hercikmartin7@gmail.com>
2022-12-01 11:20:26 +0100

    first commit

---
+++ res/js/main.js
@@ -0,0 +1,21 @@
+const route1 = document.getElementById("route1");
+const route2 = document.getElementById("route2");
+const route3 = document.getElementById("route3");
+
+const back = document.getElementById("back");
+
+route1.onclick = () => {
+  main.style.backgroundImage = "url(./res/img/servis.png)";
+  route1.style.display = "none";
+  route2.style.display = "none";
+  route3.style.display = "none";
+  back.style.display = "block";
+};
+
+back.onclick = () => {
+  main.style.backgroundImage = "url(./res/img/krizovatka.png)";
+  route1.style.display = "block";
+  route2.style.display = "block";
+  route3.style.display = "block";
+  back.style.display = "none";
+};
