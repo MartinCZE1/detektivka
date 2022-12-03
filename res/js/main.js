d9d0b41b1906dbda489e1d5a5b2ea70670cf2f98
Martin <martinhercik16@gmail.com>
2022-12-03 12:34:34 +0100

    Init

--- res/js/main.js
+++ res/js/main.js
@@ -5,17 +5,21 @@ const route3 = document.getElementById("route3");
 const back = document.getElementById("back");
 
 route1.onclick = () => {
-  main.style.backgroundImage = "url(./res/img/servis.png)";
+  servis.style.backgroundImage = "url(./res/img/servis.png)";
   route1.style.display = "none";
   route2.style.display = "none";
   route3.style.display = "none";
   back.style.display = "block";
+  cross.style.display = "none";
+  servis.style.display = "block";
 };
 
 back.onclick = () => {
-  main.style.backgroundImage = "url(./res/img/krizovatka.png)";
+  cross.style.backgroundImage = "url(./res/img/krizovatka.png)";
   route1.style.display = "block";
   route2.style.display = "block";
   route3.style.display = "block";
   back.style.display = "none";
+  cross.style.display = "block";
+  servis.style.display = "none";
 };
