881a09577179ca10183ecb08be554661e9e6b5dc
Martin <martinhercik16@gmail.com>
2022-12-07 18:14:31 +0100

    Init

--- res/js/main.js
+++ res/js/main.js
@@ -2,30 +2,47 @@ const route1 = document.getElementById("route1");
 const route2 = document.getElementById("route2");
 const route3 = document.getElementById("route3");
 
-const back = document.getElementById("back");
+const backservis = document.getElementById("backservis");
+const backhospoda = document.getElementById("backhospoda");
 
-const rakev = document.getElementById("rakev")
+const rakev = document.getElementById("rakev");
 
 route1.onclick = () => {
   servis.style.backgroundImage = "url(./res/img/servis.png)";
-  route1.style.display = "none";
-  route2.style.display = "none";
-  route3.style.display = "none";
-  back.style.display = "block";
+  backservis.style.display = "block";
   cross.style.display = "none";
   servis.style.display = "block";
+  hospoda.style.display = "none"
 };
 
-back.onclick = () => {
+route2.onclick = () => {
+  hospoda.style.backgroundImage = "url(./res/img/hospoda.png)"
+  hospoda.style.display = "block";
+  cross.style.display = "none";
+  backhospoda.style.display = "block"
+};
+
+backservis.onclick = () => {
   cross.style.backgroundImage = "url(./res/img/krizovatka.png)";
   route1.style.display = "block";
   route2.style.display = "block";
   route3.style.display = "block";
-  back.style.display = "none";
+  backservis.style.display = "none";
   cross.style.display = "block";
   servis.style.display = "none";
 };
 
+backhospoda.onclick = () => {
+  cross.style.backgroundImage = "url(./res/img/krizovatka.png)";
+  route1.style.display = "block";
+  route2.style.display = "block";
+  route3.style.display = "block";
+  backhospoda.style.display = "none";
+  cross.style.display = "block";
+  servis.style.display = "none";
+  hospoda.style.display = "none"
+};
+
 rakev.onclick = () => {
   dead.style.display = "block";
-}
+};
