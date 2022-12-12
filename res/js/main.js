42b58846a5a93d8629b88265a85a3f24f143fed1
Martin <martinhercik16@gmail.com>
2022-12-12 19:00:31 +0100

    Init

--- res/js/main.js
+++ res/js/main.js
@@ -1,3 +1,9 @@
+const vstup = document.getElementById("vstup")
+const jmeno = document.getElementById("jmeno");
+const jmenobtn = document.getElementById("jmenobtn");
+const vstuptext = document.getElementById("vstuptext");
+const hrat = document.getElementById("hrat");
+
 const route1 = document.getElementById("route1");
 const route2 = document.getElementById("route2");
 const route3 = document.getElementById("route3");
@@ -6,8 +12,28 @@ const backservis = document.getElementById("backservis");
 const backhospoda = document.getElementById("backhospoda");
 
 const rakev = document.getElementById("rakev");
+const zavritrakev = document.getElementById("zavritrakev");
 
 const doba = document.getElementById("doba");
+const muzvhospode = document.getElementById("muzvhospode");
+const zavritmuzvhospodeinfo = document.getElementById("zavritmuzvhospodeinfo");
+
+//Konec proměnných
+
+jmenobtn.onclick = () => {
+  const jmenoValue = jmeno.value;
+  vstuptext.innerText =
+    "Ahoj detektive " +
+    `${jmeno.value}` +
+    ", dnes budeš mít za úkol vyřesit vraždu majitele autoservisu. Při tvém vyšetřování narazíš na pár zvláštních lidí, okolností a předmětů a bude jen na tobě zjistit, kdo je vrah a zatknout toho pravého. Hodně štěstí.";
+  vstuptext.style.display = "block";
+  hrat.style.display = "block";
+};
+
+hrat.onclick = () => {
+  cross.style.display = "block"
+  vstup.style.display = "none"
+}
 
 route1.onclick = () => {
   servis.style.backgroundImage = "url(./res/img/servis.png)";
@@ -23,7 +49,10 @@ route2.onclick = () => {
   cross.style.display = "none";
   backhospoda.style.display = "block";
   doba.style.display = "block";
+  muzvhospode.style.display = "block";
 };
+
+//Konec křižovatky
 
 backservis.onclick = () => {
   cross.style.backgroundImage = "url(./res/img/krizovatka.png)";
@@ -46,11 +75,21 @@ backhospoda.onclick = () => {
   hospoda.style.display = "none";
   doba.style.display = "none";
   dobainfo.style.display = "none";
+  muzvhospode.style.display = "none";
+  muzvhospodeinfo.style.display = "none";
 };
+
+//Konec zpátečních tlačítek
 
 rakev.onclick = () => {
   dead.style.display = "block";
 };
+
+zavritrakev.onclick = () => {
+  dead.style.display = "none";
+};
+
+//Konec Oběti
 
 doba.onclick = () => {
   dobainfo.style.display = "block";
@@ -59,5 +98,15 @@ doba.onclick = () => {
     if ((dobainfo.style.display = "none")) {
       clearInterval(dobainterval);
     }
-  }, 1000);
+  }, 1500);
 };
+
+muzvhospode.onclick = () => {
+  muzvhospodeinfo.style.display = "block";
+};
+
+zavritmuzvhospodeinfo.onclick = () => {
+  muzvhospodeinfo.style.display = "none";
+};
+
+//Konec hospody
